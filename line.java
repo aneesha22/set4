849fd5d89d8c17a813dd4f81d1f6920e4f7e76ad
aneesha22 <42198319+aneesha22@users.noreply.github.com>
2018-08-16 08:55:49 -0700

    Create line.java

---
+++ line.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int line=sc.nextInt();
+		int lines=3;
+		for(int i=0;i<line;i++)
+		{
+			if(line==0)
+		    line++;
+			
+		}
+		System.out.println("number of lines:"+lines);
+	}
+}
